795522ce8e86e5207ecbaab7f3cc46c27c512c86
mannumishra <mannumishra2207@outlook.com>
2024-08-29 11:33:28 +0530

    add chnages

---
+++ frontend/src/Component/ThankYou/ThankYou.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ThankYou.css';
+
+const ThankYou = () => {
+    return (
+        <div className="thank-you-container">
+            <img src="https://mediaman.in/static/media/logo.94585481204bda4cb964.png" alt="Mediaman Logo" className="thank-you-logo"/>
+            <h1>Thank You!</h1>
+            <p>Thank you for reaching out to Mediaman. We have received your message and will get back to you shortly.</p>
+            <a href="https://mediaman.in" className="thank-you-button">Return to Home</a>
+        </div>
+    );
+};
+
+export default ThankYou;
